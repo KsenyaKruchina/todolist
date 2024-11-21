749e020f72d0f71b2cd6c99e6c5ff5b6d7df4169
KsenyaKruchina <kseniyakruchina@gmail.com>
2024-11-22 01:11:17 +0500

    9 step

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,33 +1,23 @@
+import React from 'react';
+import TodoListItem from './todo-list-item';
+
 const TodoList = ({ todos }) => {
+  const elements = todos.map((item, index) => {
     return (
-      <ul>
-        <li>
-          <TodoListItem 
-            label={todos[0].label} 
-            important={todos[0].important} 
-          />
-        </li>
-        <li>
-          <TodoListItem 
-            label={todos[1].label} 
-            important={todos[1].important} 
-          />
-        </li>
-        <li>
-          <TodoListItem 
-            label={todos[2].label} 
-            important={todos[2].important} 
-          />
-        </li>
-        <li>
-          <TodoListItem 
-            label={todos[3].label} 
-            important={todos[3].important} 
-          />
-        </li>
-      </ul>
+      <li>
+        <TodoListItem 
+          label={item.label} 
+          important={item.important} 
+        />
+      </li>
     );
-  };
-  
-  export default TodoList;
-  
+  });
+
+  return (
+    <ul>
+      {elements}
+    </ul>
+  );
+};
+
+export default TodoList;
