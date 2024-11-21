a775956860b45dd9f60119cca9cfd1f1e9120b90
KsenyaKruchina <kseniyakruchina@gmail.com>
2024-11-22 02:26:18 +0500

    12 step

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AppHeader from './components/app-header';
+import './index.css'; 
+import AppHeader from './components/header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
 const App = () => {
+  
   const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1},
-    { label: 'Implement React Application', important: true, id: 2},
-    { label: 'Make notes from your study', important: false, id: 3 },
-    { label: 'Do not drink alcohol', important: false, id: 4 },
+    { id: 1, label: 'Drink Coffee', important: false },
+    { id: 2, label: 'Implement React Application', important: true },
+    { id: 3, label: 'Make notes from your study', important: false },
+    { id: 4, label: 'Do not drink alcohol', important: false },
   ];
 
   return (
-    <div>
+    <div className="container mt-5">
       <AppHeader />
       <SearchPanel />
       <TodoList todos={todoData} />
