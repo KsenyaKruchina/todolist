bcb2685b59507357cfe31eced1499652d73ef2e0
KsenyaKruchina <kseniyakruchina@gmail.com>
2024-11-22 00:59:47 +0500

    6 step

--- src/index.js
+++ src/index.js
@@ -1,46 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const TodoList = () => {
-  const items = ['Install React', 'Study React', 'Use React', 'Build React App'];
-
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
-      <li>{items[3]}</li>
-    </ul>
-  );
-};
-
-const SearchPanel = () => {
-  const searchText = "Type here to search";
-  const searchStyle = {
-    fontSize: '20px'
-  };
-
-  return (
-    <input
-      style={searchStyle}
-      placeholder={searchText}
-      disabled={true}
-    />
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-  const isLoggedIn = false;
-  const loginBox = <span>Log in to the system</span>;
-  const welcomeBox = <span>Welcome Back!</span>;
-
   return (
     <div>
-      {isLoggedIn ? welcomeBox : loginBox}
       <AppHeader />
       <SearchPanel />
       <TodoList />
