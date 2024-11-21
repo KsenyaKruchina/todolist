8b31ff4bb20d49da3c1bfa6c02274566bd63b65f
KsenyaKruchina <kseniyakruchina@gmail.com>
2024-11-22 00:28:03 +0500

    3 step

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Install React</li>
+      <li>Study React</li>
+      <li>Use React</li>
+      <li>Build React App</li>
+    </ul>
+  </div>
 );
 
-console.log('Hello Kseniya');
-
-reportWebVitals(console.log);
+ReactDOM.render(
+  element,
+  document.getElementById("root")
+);
