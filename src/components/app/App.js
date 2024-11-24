903a7191a99e84ce6a8da59237a165613c0d7b35
KsenyaKruchina <kseniyakruchina@gmail.com>
2024-11-25 00:14:19 +0500

    26 step

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -17,9 +17,10 @@ export default class App extends Component {
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((element) => element.id === id);
-      const before = todoData.slice(0, idx); 
-      const after = todoData.slice(idx + 1); 
-      const newArray = [...before, ...after]; 
+      const newArray = [
+        ... todoData.slice(0, idx),
+        ... todoData.slice(idx + 1) 
+      ];
 
       return {
         todoData: newArray, 
